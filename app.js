284f2fb01a851cee8573791f24b206ea80128f08
Felipe Oliveira <felipe.hsoliveira@outlook.com>
2022-06-03 00:41:14 -0300

    teste do teste

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! é nooooiizzzzz começando os trabalhos'));
+app.get('/', (req, res) => res.send('Hello World! agora foi manolo'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
